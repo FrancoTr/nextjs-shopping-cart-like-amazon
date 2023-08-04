3ac88ad48fa1580736e0d10581ebda30f47c1ccf
Franco Traverso <franco-t20@outlook.com>
2023-08-04 20:28:44 -0300

    Fix typo in import

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSliceReducer from "./slices/cartSlice.js";
+import cartSliceReducer from "./slices/cartSlice";
 
 export const store = configureStore({
   reducer: {
