e8e8ce418d52f1622300fde45cbab2021f42c1c2
Franco Traverso <franco-t20@outlook.com>
2023-08-24 17:51:35 -0300

    Add shipping form

--- src/app/shipping/page.js
+++ src/app/shipping/page.js
@@ -34,6 +34,49 @@ export default function ShippingAddressPage() {
   return (
     <div>
       <CheckoutWizard />
+      <form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
+        <h1 className='mb-4 text-xl'>Shipping Address</h1>
+        <div className='mb-4'>
+          <label htmlFor='fullName'>Full Name</label>
+          <input
+            className='w-full'
+            id='fullName'
+            autoFocus
+            {...register("fullName", { required: "Please enter full name" })}
+          />
+          {errors.fullName && <div className='text-red-500'>{errors.fullName.message}</div>}
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='address'>Address</label>
+          <input
+            className='w-full'
+            id='address'
+            {...register("address", {
+              required: "Please enter address",
+              minLength: { value: 3, message: "Address is more than 2 chars" },
+            })}
+          />
+          {errors.address && <div className='text-red-500'>{errors.address.message}</div>}
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='postalCode'>Postal Code</label>
+          <input
+            className='w-full'
+            id='postalCode'
+            {...register("postalCode", { required: "Please enter postal code" })}
+          />
+          {errors.postalCode && <div className='text-red-500'>{errors.postalCode.message}</div>}
+          <div className='mb-4'>
+            <label htmlFor='country'>Country</label>
+            <input
+              className='w-full'
+              id='country'
+              {...register("country", { required: "Please enter country" })}
+            />
+            {errors.country && <div className='text-red-500'>{errors.country.message}</div>}
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
